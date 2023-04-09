5cd2c71cdf7bf1a5698ddf12d9dba2e166883bc0
Aaron Diable <aarond.diable@gmail.com>
2023-04-09 19:40:52 +0100

    Rewrote controllers to use consitent try/catch and not mixing try/catch and then/catch

--- controller/postRootController.js
+++ controller/postRootController.js
@@ -2,66 +2,69 @@ const bcrypt = require('bcrypt');
 const SALT = 10;
 const User = require('../models/users');
 
-module.exports =  {
+module.exports = {
     login: async (req, res) => {
-        const { email, password } = req.body;
+        try {
+            const { email, password } = req.body;
 
-        let user = await User.findOne({ email })
+            let user = await User.findOne({ email });
 
-
-        if(!user) {
-            return res.json({
-                error: 'User not found'
-            })
-        }
-
-        const isMatch = await bcrypt.compare(password, user.password);
-
-        if(!isMatch) {
-            return res.json({
-                error: 'Password is incorrect'
-            })
-        }
-
-        console.log('User logged in');
-
-        res.redirect('/');
-    },
-    signin: async (req, res) => {
-        const { fname, lname, username, email, password, password2, newsletter } = req.body;
-
-        if(password !== password2) {
-            return res.json({
-                error: 'Passwords do not match'
-            })
-        }
-
-        let getNewsletter = false;
-        if(newsletter === 'on') {
-            getNewsletter = true;
-        }
-
-        const hashedPassword = await bcrypt.hash(password, SALT);
-
-        const newUser = User({
-            username,
-            email,
-            password: hashedPassword,
-            firstName: fname,
-            lastName: lname,
-            newsletter
-        });
-
-        await newUser.save()
-            .then((data) => {
-                console.log(data);
-                res.redirect('/');
-            })
-            .catch(err => {
-                console.log(err);
-                res.json({
-                    error: err
+            if (!user) {
+                return res.json({
+                    error: 'User not found'
                 });
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if (!isMatch) {
+                return res.json({
+                    error: 'Password is incorrect'
+                });
+            }
+
+            console.log('User logged in');
+
+            res.redirect('/');
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal server error' });
+        }
+    },
+
+    signin: async (req, res) => {
+        try {
+            const { fname, lname, username, email, password, password2, newsletter } = req.body;
+
+            if (password !== password2) {
+                return res.json({
+                    error: 'Passwords do not match'
+                });
+            }
+
+            let getNewsletter = false;
+            if (newsletter === 'on') {
+                getNewsletter = true;
+            }
+
+            const hashedPassword = await bcrypt.hash(password, SALT);
+
+            const newUser = User({
+                username,
+                email,
+                password: hashedPassword,
+                firstName: fname,
+                lastName: lname,
+                newsletter
             });
+
+            const data = await newUser.save();
+            console.log(data);
+
+            res.redirect('/');
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal server error' });
+        }
     }
-}
+};
--- routes/root.js
+++ routes/root.js
@@ -6,13 +6,13 @@ const getRootController = require('../controller/getRootController.js');
 const postRootController = require('../controller/postRootController.js');
 
 // Get Routes for rendering pages
-router.get('/', getRootController.index);
+router.get('/', getRootController.renderHomePage);
 
-router.get('/about', getRootController.about)
+router.get('/about', getRootController.renderAboutPage)
 
-router.get('/contact', getRootController.contact);
+router.get('/contact', getRootController.renderContactPage);
 
-router.get('/signin', getRootController.signin);
+router.get('/signin', getRootController.renderSignInPage);
 
 
 // Post Rotes for handling form submissions and returning data
